d370fb28c184108ed8d7484447eaa2f4bd078946
roynatech2544 <whiteshell2544@naver.com>
2023-08-10 16:56:58 +0900

    tb128fu: Enable Virtual A/B support

--- device.mk
+++ device.mk
@@ -21,6 +21,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Virtual A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
 LOCAL_PATH := device/lenovo/TB128FU
 
 # define hardware platform
